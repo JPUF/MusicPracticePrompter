2f092f2c7b066f7f8f9583e84cf21f5d0dba3fe5
JPUF <2009bennettj@gmail.com>
2019-08-01 15:08:29 +0100

    failed attempt to navigate to prompt fragment

--- app/src/main/java/com/jlbennett/musicpracticeprompter/TitleFragment.kt
+++ app/src/main/java/com/jlbennett/musicpracticeprompter/TitleFragment.kt
@@ -1,11 +1,13 @@
 package com.jlbennett.musicpracticeprompter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.jlbennett.musicpracticeprompter.databinding.FragmentTitleBinding
 
 
@@ -16,6 +18,12 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        //TODO wy doesn't this function navigate to the prompt fragment...
+        binding.titleButton.setOnClickListener { view: View ->
+            Log.v("button", "click listener")
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_promptFragment)
+        }
 
         return binding.root
     }
