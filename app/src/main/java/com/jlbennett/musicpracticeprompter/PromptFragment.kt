a8cf790bb9c0a0fec1b6eaa001a24c133aeacc12
JPUF <2009bennettj@gmail.com>
2019-08-02 16:31:03 +0100

    A 'next' button to select a new key prompt.

--- app/src/main/java/com/jlbennett/musicpracticeprompter/PromptFragment.kt
+++ app/src/main/java/com/jlbennett/musicpracticeprompter/PromptFragment.kt
@@ -10,11 +10,21 @@ import com.jlbennett.musicpracticeprompter.databinding.FragmentPromptBinding
 
 class PromptFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentPromptBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_prompt, container, false)
+        val binding: FragmentPromptBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_prompt, container, false)
+
+        binding.nextButton.setOnClickListener {
+            val roots = listOf("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#")
+            val mode = listOf("Major", "Minor")
+            binding.promptText.text = "${roots.random()}\n${mode.random()}"
+        }
+
         return binding.root
     }
 }
