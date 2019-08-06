0624a9052fc6d96db58f4ff668a0633992463123
JPUF <2009bennettj@gmail.com>
2019-08-06 12:45:23 +0100

    Implemented note reminder button.

--- app/src/main/java/com/jlbennett/musicpracticeprompter/PromptFragment.kt
+++ app/src/main/java/com/jlbennett/musicpracticeprompter/PromptFragment.kt
@@ -1,6 +1,7 @@
 package com.jlbennett.musicpracticeprompter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import com.jlbennett.musicpracticeprompter.databinding.FragmentPromptBinding
 class PromptFragment : Fragment() {
 
     private lateinit var keyArray: Array<String>
+    private lateinit var currentKey: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,10 +29,30 @@ class PromptFragment : Fragment() {
         keyArray = resources.getStringArray(args.preset)
 
         binding.promptText.setOnClickListener {
-            val keyPrompt: String = keyArray.random()
-            binding.promptText.text = keyPrompt.replace(' ','\n')
+            binding.noteReminderLayout.visibility = View.VISIBLE
+            binding.noteReminderText.text = ""
+            currentKey = keyArray.random()
+            binding.promptText.text = currentKey.replace(' ', '\n')
+            //Toast.makeText(context, "Notes: ${getNotesFromKey(keyPrompt)}", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.noteReminderButton.setOnClickListener {
+            binding.noteReminderText.text = getNotesFromKey(currentKey)
         }
 
         return binding.root
+    }
+
+    private fun getNotesFromKey(key: String): String {
+        var notes: String = "notes"
+        val noteLookupArray = resources.getStringArray(R.array.noteLookupArray)
+        for(pair in noteLookupArray) {
+            val splitResult = pair.split("\\|".toRegex(), 2)
+            if (splitResult[0] == key) {
+                notes = splitResult[1]
+                break
+            }
+        }
+        return notes
     }
 }
